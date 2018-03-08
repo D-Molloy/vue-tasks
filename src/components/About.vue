8391b8a1bf8305b191465ffaaddcccb8dcf62a06
Denis Molloy <denis.molloy@gmail.com>
2018-03-07 17:37:12 -0800

    tweaks

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
     <div class="about">
         <!-- <h1>Hi, {{their_name}}, why should you Denny-Do?</h1> -->
         <h1>Why should you Denny-Do?</h1>
-        <p>You need help tracking and completing tasks, but who has the time and available pocket space to carry around a pad of paper and writing utensil?  That's why we created the Denny-Do List.  Save all of your tasks in once convenient location and mark them complete as you go!</p>
+        <p>You need help tracking and completing tasks, but who has the time and available pocket space to carry around a pad of paper and writing utensil?  That's why we created Denny-Do List.  Save all of your tasks in one convenient location and mark them complete as you go!</p>
     </div>
 </template>
 
